c0c565bbee97d6b8d7b29268d395fca72a013596
Samsegun <oyebadesamsegun@gmail.com>
2023-07-14 19:48:26 +0100

    basic auth middleware

--- src/router.ts
+++ src/router.ts
@@ -5,29 +5,34 @@ const router = Router();
 /**
  * Product routes
  */
-router.get("/product", (res, req) => {
-    res.json({ message: "product" });
+router.get("/product", (req, res) => {
+    res.json({ message: "message" });
 });
-
 router.get("/product/:id", () => {});
-
 router.post("/product", () => {});
-
 router.put("/product/:id", () => {});
-
 router.delete("/product/:id", () => {});
 
 /**
  * Update routes
  */
-router.get("/product", (res, req) => {
+router.get("/update", (req, res) => {
     res.json({ message: "update" });
 });
-
 router.get("/update/:id", () => {});
-
 router.post("/update", () => {});
-
 router.put("/update/:id", () => {});
-
 router.delete("/update/:id", () => {});
+
+/**
+ * UpdatePoint routes
+ */
+router.get("/updatepoint", (req, res) => {
+    res.json({ message: "update point" });
+});
+router.get("/updatepoint/:id", () => {});
+router.post("/updatepoint", () => {});
+router.put("/updatepoint/:id", () => {});
+router.delete("/updatepoint/:id", () => {});
+
+export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,28 @@
 import express from "express";
+import router from "./router";
 import path from "path";
+import morgan from "morgan";
+import cors from "cors";
+import { protect } from "./modules/auth";
 
 const app = express();
+
+// const customLogger = message => (req, res, next) => {
+//     console.log(message);
+//     next();
+// };
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use(customLogger("custom logger active"));
+
 app.get("/", (req, res) => {
     console.log("hello from express");
-
-    // res.status(200);
     res.sendFile(path.resolve("./src/index.html"));
-    // res.json({ message: "hello" });
 });
+
+app.use("/api", protect, router);
 
 export default app;
